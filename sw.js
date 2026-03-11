3f9e0c6a3698918435496b22dad6d804577370b7
rozaytech <adamgyadamo@gmail.com>
2026-03-11 23:03:29 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,9 @@
-const CACHE_NAME = 'rozay-noc-v3';
+const CACHE_NAME = 'rozay-tech-v4';
 const assets = [
   './',
+  './index.html',
+  './style.css',
+  './script.js',
   './login.html',
   './dashboard.html',
   './dash-style.css',
@@ -9,9 +12,27 @@ const assets = [
 ];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
 });
